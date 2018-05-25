887ae9e53c26748e11dc27fb9fb5a463f538fbcc
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-05-25 15:57:52 +0500

    Updated.

---
+++ Version 2 (ProcessingJS)/Libraries used/js/shapes.js
@@ -0,0 +1,82 @@
+//Polygon
+function Polygon(array_of_vectors)
+{
+	this.array_of_vectors = array_of_vectors;
+	this.col = 135;
+
+	fill(this.col);
+	beginShape();
+	var i = 0;
+	for(i=0; i<this.array_of_vectors.length; i++)
+	{
+		vertex(array_of_vectors[i].x, array_of_vectors[i].y);
+	}
+	endShape();
+
+	this.color = function(c)
+	{
+		//clear();
+		this.col = c;
+		fill(this.col);
+		beginShape();
+		var i = 0;
+		for(i=0; i<this.array_of_vectors.length; i++)
+		{
+			vertex(this.array_of_vectors[i].x, this.array_of_vectors[i].y);
+		}
+		endShape();
+	};
+}
+
+//PolyLine
+function PolyLine(array_of_vectors)
+{
+	this.array_of_vectors = array_of_vectors;
+	this.col = 0;
+
+	stroke(this.col);
+	strokeWeight(2);
+
+	var i=0;
+	for(i=0; i<this.array_of_vectors.length - 1; i++)
+	{
+		line(this.array_of_vectors[i].x, this.array_of_vectors[i].y, this.array_of_vectors[i+1].x, this.array_of_vectors[i+1].y);
+	}
+	
+	this.color = function(c)
+	{
+		this.col = c;
+		stroke(this.col);
+		strokeWeight(2);
+
+		var i=0;
+		for(i=0; i<this.array_of_vectors.length - 1; i++)
+		{
+			line(this.array_of_vectors[i].x, this.array_of_vectors[i].y, this.array_of_vectors[i+1].x, this.array_of_vectors[i+1].y);
+		}
+	};
+}
+
+//Tooltip
+function Tooltip(title, startX, startY, width, height)
+{
+	this.title = title;
+	this.startX = startX;
+	this.startY = startY;
+	this.width = width;
+	this.height = height;
+
+	this.show = function()
+	{
+		fill(255, 0, 0);
+		rect(this.startX, this.startY, this.width, this.height);
+		fill(255);
+		textSize(15);
+		text(this.title, this.startX+20, this.startY+20);
+	};
+
+	this.hide = function()
+	{
+		clear();
+	};
+}
---
+++ Version 2 (ProcessingJS)/Libraries used/js/sketch.js
@@ -0,0 +1,95 @@
+/*Base Map*/
+//Polygon
+var array_polygons_basemap = [];
+var pointer_polygons_basemap = null;
+
+//PolyLine
+var array_lines_basemap = [];
+var pointer_lines_basemap = null;
+
+// -----------------------------------------------------------------------
+
+/*Plot Profile*/
+//Polygon
+var array_polygons_plotprofile = [];
+var pointer_polygons_plotprofile = null;
+
+//PolyLine
+var array_lines_plotprofile = [];
+var pointer_lines_plotprofile = null;
+
+// -----------------------------------------------------------------------
+
+/*Water Lines*/
+//Polygon
+var array_polygons_waterlines = [];
+var pointer_polygons_waterlines = null;
+
+//PolyLine
+var array_lines_waterlines = [];
+var pointer_lines_waterlines = null;
+
+// -----------------------------------------------------------------------
+
+/*Gas Lines*/
+//Polygon
+var array_polygons_gaslines = [];
+var pointer_polygons_gaslines = null;
+
+//PolyLine
+var array_lines_gaslines = [];
+var pointer_lines_gaslines = null;
+
+// -----------------------------------------------------------------------
+
+/*Sewage Lines*/
+//Polygon
+var array_polygons_sewagelines = [];
+var pointer_polygons_sewagelines = null;
+
+//PolyLine
+var array_lines_sewagelines = [];
+var pointer_lines_sewagelines = null;
+
+// -----------------------------------------------------------------------
+
+//On-Off Tracker
+var num = 0;	//Make an array instead
+
+
+function setup()
+{
+	createCanvas(900, 900);
+	background(255);
+
+	/*basmapData();
+	plotprofileData();*/
+	waterlinesData();
+	/*gaslinesData();
+	sewagelinesData();*/
+}
+
+				
+function draw()
+{	
+	clear();
+	background(255);
+
+	document.getElementById('btnOn').onclick = function()
+	{	
+		
+		console.log("Button clicked");
+		num = 1; 		
+	}
+
+	document.getElementById('btnOff').onclick = function()
+	{
+		num = 0;
+		clear();
+	}
+
+	if(num == 1)
+	{
+		waterlines();	
+	}
+}
