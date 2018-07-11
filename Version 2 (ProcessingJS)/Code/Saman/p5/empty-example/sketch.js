d964f390c57098d10b0b3a248c80c77badca3c33
Saman Gaziani <sg02494@st.habib.edu.pk>
2018-07-10 23:25:02 -0700

    10/7/18

--- Version 2 (ProcessingJS)/Code/Saman/p5/empty-example/sketch.js
+++ Version 2 (ProcessingJS)/Code/Saman/p5/empty-example/sketch.js
@@ -1,27 +1,38 @@
-var X = 814.13
-var Y = 10
-lengthofLine = 573.64/20
-var checker = 1
+var x1 = 200;
+var y1 = 200;
+var x2 = 400;
+var y2 = 400;
+var shifts = 10;
+var lengthofLine = Math.sqrt(((X1-X2)*(X1-X2))((Y1-Y2)*(Y1-Y2)))/20;
+var changeinx = (X1-X2)/20 ;
+var checker = 1;
+var slope = (y2-y1)/(x2-x1); 
+
 function setup() {
   createCanvas(1000,1000)
 }
 
 function draw() 
 {
-  background(0,255,0)
-  if (Y == 0){
-    checker = 0
-  }
-  if (checker == 1){
-  for (counter=0;counter<lengthofLine;counter++){
-    line(814.13,805.51+Y-(20*counter),814.13,795.51+Y-(counter*20))
-  }
-  Y--
-  }
-  else{
-  Y = 10
-  checker = 1
-  }
+  clear();
+					background(123, 123, 223);	
+					stroke(0);
 
+					var i=10;
 
+					
+					line(x1, y1, (x1 + 10), y1 + ((x1 + 10) - x1)*slope);
+					line((x1 + 20), y1 + ((x1 + 20) - x1)*slope, (x1 + 30), y1 + ((x1 + 30) - x1)*slope);
+
+					x1 = x1 + 10;
+					y1 = y1 + ((x1 + 10) - x1)*slope;
+					
+
+					if(x1 > x2)
+					{
+						x1 = 200;
+						y1 = 200;
+						x2 = 400;
+						y2 = 400;
+					}
 }
