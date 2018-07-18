305dbc04c50c60e7b973689b15ccfac624a7b9b8
Saman Gaziani <sg02494@st.habib.edu.pk>
2018-07-17 20:05:52 -0700

    Straight and slant dotted lines

--- Version 2 (ProcessingJS)/Code/Saman/p5/empty-example/sketch.js
+++ Version 2 (ProcessingJS)/Code/Saman/p5/empty-example/sketch.js
@@ -30,3 +30,34 @@ function draw()
   }
 
 }
+
+
+
+
+var X = 814.13
+var Y = 10
+lengthofLine = (805.51-573.64)/20
+var checker = 1
+function setup() {
+  createCanvas(1000,1000)
+}
+
+function draw() 
+{
+  background(0,255,0)
+  if (Y == 0){
+    checker = 0
+  }
+  if (checker == 1){
+  for (counter=0;counter<lengthofLine;counter++){
+    line(X,805.51+Y-(20*counter),X,795.51+Y-(counter*20))
+  }
+  Y--
+  }
+  else{
+  Y = 10
+  checker = 1
+  }
+
+
+}
